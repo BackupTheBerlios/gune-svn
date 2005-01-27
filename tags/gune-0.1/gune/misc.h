0453f4f75efb417d77d9abc077beaf5f47a72df3
nvie <nvie@789b94cb-67db-0310-a52b-8eb14391789c>
2005-01-27 13:22:30 +0000

    TAG: Gune 0.1. w00t!

---
+++ tags/gune-0.1/gune/misc.h
@@ -0,0 +1,95 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2004 Peter Bex and Vincent Driessen
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the names of Peter Bex or Vincent Driessen nor the names of any
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY PETER BEX AND VINCENT DRIESSEN AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * \brief Miscellaneous functionality interface.
+ *
+ * \file misc.h
+ * Loose odds and ends which don't really belong anywhere.
+ */
+#ifndef GUNE_MISC_H
+#define GUNE_MISC_H
+
+#include <gune/types.h>
+
+/**
+ * Swap two variables' contents.
+ * \note
+ * This only works for basic types, and the variables must be of the same type.
+ *
+ * \param t  The type of the variables.
+ * \param a  The first variable.
+ * \param b  The second variable.
+ *
+ * \hideinitializer
+ */
+#define SWAP(t,a,b)	{ t __GUNE_SWAP_TMP = (a); (a) = (b); (b) = __GUNE_SWAP_TMP; }
+
+/**
+ * Determine the maximum of two variables.
+ * \note
+ * This only works for basic types.
+ *
+ * \param a  The first variable.
+ * \param b  The second variable.
+ *
+ * \return   The maximum of \p a and \p b, or \p b if they're equal.
+ *
+ * \hideinitializer
+ */
+#define MAX(a,b)	((a) > (b) ? (a) : (b))
+
+/**
+ * Determine the minimum of two variables.
+ * \note
+ * This only works for basic types.
+ *
+ * \param a  The first variable.
+ * \param b  The second variable.
+ *
+ * \return   The minimum of \p a and \p b, or \p b if they're equal.
+ *
+ * \hideinitializer
+ */
+#define MIN(a,b)	((a) < (b) ? (a) : (b))
+
+int ptr_eq(gendata, gendata);
+int num_eq(gendata, gendata);
+int posnum_eq(gendata, gendata);
+int sym_eq(gendata, gendata);
+unsigned int ptr_hash(gendata, unsigned int);
+unsigned int num_hash(gendata, unsigned int);
+unsigned int posnum_hash(gendata, unsigned int);
+unsigned int sym_hash(gendata, unsigned int);
+
+extern void * const CONST_PTR;
+
+#endif /* GUNE_MISC_H */
---
+++ tags/gune-0.1/release.sh
@@ -0,0 +1,175 @@
+#!/bin/sh
+#
+# $Id$
+#
+# Script for creating a release
+#
+
+PROJECT="gune"
+PROJECT_UPPER="GUNE"
+SHLIB_VERSION="${PROJECT}/shlib_version"
+
+VERSION_HEADER="${PROJECT}/version.h"
+VERSION_MAJOR="${PROJECT_UPPER}_MAJOR_VERSION"
+VERSION_MINOR="${PROJECT_UPPER}_MINOR_VERSION"
+
+CREATE_TAR="tar czf"
+RM="rm -rf"
+MAKE=make
+CAT=cat
+GREP=grep
+SED="sed -E"
+EXPR=expr
+MV="mv -f"
+
+XARGS_RM="xargs rm -rf"
+
+
+usage ()
+{
+	echo "Usage: release.sh [options]"
+	echo "Options:"
+	echo "-r dir  Make release tarball from REPOS_PATH/dir"
+	echo "-v      Bump minor version number (and update header)"
+	echo "-V      Bump major version number (and update header)"
+	echo "-H      Update header files to reflect shlib_version"
+	echo "-h      Show this help"
+	echo "-p      Print version numbers and repos"
+	echo
+	echo "release.sh will look at ${SHLIB_VERSION} for major/minor"
+	echo "version numbers.  For svn, it will call svn info on the current"
+	echo "working directory and grab the URL line"
+}
+
+getrepos ()
+{
+	REPOS=`svn info | ${GREP} URL | ${SED} "s/URL:[[:space:]]*//" | ${SED} "s,/(trunk|branches/.*|tags/.*),,`
+}
+
+getversion ()
+{
+	MAJOR=`${GREP} major ${SHLIB_VERSION} | ${SED} "s/[^0-9]*([0-9]+)[^0-9]*/\1/"`
+	MINOR=`${GREP} minor ${SHLIB_VERSION} | ${SED} "s/[^0-9]*([0-9]+)[^0-9]*/\1/"`
+	VERSIONSTRING="${MAJOR}.${MINOR}"
+	BASENAME="${PROJECT}-${VERSIONSTRING}"
+}
+
+bumpmajor ()
+{
+	getversion
+	NEWMAJOR=`${EXPR} ${MAJOR} + 1`
+	${SED} "s/major[[:space:]]*=[[:space:]]*[0-9]+[^0-9]*/major=${NEWMAJOR}/;s/minor[[:space:]]*=[[:space:]]*[0-9]+[^0-9]*/minor=0/" ${SHLIB_VERSION} > ${SHLIB_VERSION}.new || exit
+	${MV} ${SHLIB_VERSION}.new ${SHLIB_VERSION} || exit
+}
+
+bumpminor ()
+{
+	getversion
+	NEWMINOR=`${EXPR} ${MINOR} + 1`
+	${SED} "s/minor[[:space:]]*=[[:space:]]*[0-9]+[^0-9]*/minor=${NEWMINOR}/" ${SHLIB_VERSION} > ${SHLIB_VERSION}.new || exit
+	${MV} ${SHLIB_VERSION}.new ${SHLIB_VERSION} || exit
+}
+
+updateheader ()
+{
+	getversion
+	${SED} "s/(.*${VERSION_MAJOR}[^0-9]*)[0-9]+([^0-9]*)/\1${MAJOR}\2/;s/(.*${VERSION_MINOR}[^0-9]*)[0-9]+([^0-9]*)/\1${MINOR}\2/" ${VERSION_HEADER} > ${VERSION_HEADER}.new
+	${MV} ${VERSION_HEADER}.new ${VERSION_HEADER}
+}
+
+printinfo ()
+{
+	getversion
+	getrepos
+	echo "Major: ${MAJOR}"
+	echo "Minor: ${MINOR}"
+	echo "Version string: ${VERSIONSTRING}"
+	echo "Repos: ${REPOS}"
+}
+
+#
+# Remove developer-only stuff (assumes we are in the project's dir)
+#
+cleanup ()
+{
+	${SED} "/^(#.*)?$/d" devel-files | ${XARGS_RM} || exit
+	${RM} devel-files || exit
+}
+
+maketar ()
+{
+	getrepos
+	getversion
+	cd .. || exit
+	svn export "${REPOS}/${dir}" "${BASENAME}" || exit
+
+	cd ${BASENAME} || exit
+	${MAKE} release || exit
+	cleanup
+	cd .. || exit
+	${CREATE_TAR} "${BASENAME}.tar.gz" "${BASENAME}" || exit
+}
+
+
+#
+# Shell main
+#
+
+# No args?  Just show usage
+if [ $# -eq 0 ]
+then
+	usage
+	exit
+fi
+
+#
+# Init all to 0
+#
+tar=0
+min=0
+maj=0
+upd=0
+inf=0
+
+
+while getopts pr:vVHh opt
+do
+	case "${opt}" in
+		r) tar=1; dir=${OPTARG} ;;
+		v) min=1; upd=1 ;;
+		V) maj=1; upd=1 ;;
+		H) upd=1 ;;
+		p) inf=1 ;;
+		h) usage; exit ;;
+		?) usage; exit ;;
+	esac
+done
+
+#
+# Shift arguments beyond options, so $1 will be the arg after the last option
+#
+shift `${EXPR} ${OPTIND} - 1`
+
+#
+# Order is important here (eg, don't printinfo before we bumpmajor/bumpminor)
+#
+if [ $tar -eq 1 ]
+then
+	maketar
+fi
+if [ $min -eq 1 ]
+then
+	bumpminor
+fi
+if [ $maj -eq 1 ]
+then
+	bumpmajor
+fi
+if [ $upd -eq 1 ]
+then
+	updateheader
+fi
+if [ $inf -eq 1 ]
+then
+	printinfo
+fi
